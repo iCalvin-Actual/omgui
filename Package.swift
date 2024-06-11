97971b12e460ffdded39c2b26aba246d5a49bbd7
Calvin <me@iCalvin.dev>
2024-06-10 20:00:24 -0400

    Set up warnings

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -29,7 +29,9 @@ let package = Package(
                 .product(name: "MarkdownUI", package: "swift-markdown-ui"),
                 "MarkupEditor",
                 "Ink"
-            ]),
+            ],
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
+        ),
         .testTarget(
             name: "omguiTests",
             dependencies: ["omgui"]),
